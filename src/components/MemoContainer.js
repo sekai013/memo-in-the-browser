32cc458a4262d604656b08302924d9fb903ac6a3
sekai013 <sekai013@gmail.com>
2016-05-22 03:19:53 +0900

    add memocontainer component

---
+++ src/components/MemoContainer.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import MemoNode from './MemoNode.js'
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    memos: state.memos
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {}
+}
+
+class MemoContainer extends Component {
+  static get propTypes () {
+    return {
+      memos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        position: PropTypes.shape({
+          top: PropTypes.number.isRequired,
+          left: PropTypes.number.isRequired
+        }).isRequired,
+        text: PropTypes.string.isRequired
+      })).isRequired
+    }
+  }
+
+  render () {
+    const memoNodes = this.props.memos.map((memo, i) => {
+      return <MemoNode key={i} onClick={this.props.onClick} {...memo} />
+    })
+    return (
+      <div className='memoContainer'>
+        {memoNodes}
+      </div>
+    )
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MemoContainer)
