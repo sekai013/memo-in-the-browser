671e36ecaee2ea15647f7a3319438c6955470973
sekai013 <sekai013@gmail.com>
2016-06-17 15:14:58 +0900

    implement submenu about memocolor

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,11 +2,19 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { hideMenu } from '../actions/Menu.js'
 import { switchExtension } from '../actions/Extension.js'
+import { setLineColor, setBackgroundColor } from '../actions/Color.js'
+
+const SubMenuType = {
+  LineColor: 'LINE_COLOR',
+  BackgroundColor: 'BACKGROUND_COLOR'
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
     isVisibleMenu: state.isVisibleMenu,
-    isExtensionEnabled: state.isExtensionEnabled
+    isExtensionEnabled: state.isExtensionEnabled,
+    lineColor: state.subMenu.lineColor,
+    backgroundColor: state.subMenu.backgroundColor
   }
 }
 
@@ -17,6 +25,12 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     },
     switchExtension: () => {
       dispatch(switchExtension())
+    },
+    setLineColor: (color) => {
+      dispatch(setLineColor(color))
+    },
+    setBackgroundColor: (color) => {
+      dispatch(setBackgroundColor(color))
     }
   }
 }
@@ -26,9 +40,19 @@ class Menu extends Component {
     return {
       isVisibleMenu: PropTypes.bool.isRequired,
       isExtensionEnabled: PropTypes.bool.isRequired,
+      lineColor: PropTypes.string.isRequired,
+      backgroundColor: PropTypes.string.isRequired,
       hideMe: PropTypes.func.isRequired,
-      switchExtension: PropTypes.func.isRequired
+      switchExtension: PropTypes.func.isRequired,
+      setLineColor: PropTypes.func.isRequired,
+      setBackgroundColor: PropTypes.func.isRequired
     }
+  }
+
+  componentDidMount () {
+    this.setState({
+      subMenuType: null
+    })
   }
 
   getExtensionSwitcher () {
@@ -45,6 +69,89 @@ class Menu extends Component {
     )
   }
 
+  getLineColorSelector () {
+    const style = {
+      height: '30px',
+      width: '30px',
+      border: '1px solid #ccc',
+      backgroundColor: this.props.lineColor
+    }
+    const onClickHandler = (event) => {
+      this.setState({ subMenuType: SubMenuType.LineColor })
+    }
+    return (
+      <div>
+        <p>Line Color</p>
+        <div style={style} onClick={onClickHandler}>
+        </div>
+      </div>
+    )
+  }
+
+  getBackgroundColorSelector () {
+    const style = {
+      height: '30px',
+      width: '30px',
+      border: '1px solid #ccc',
+      backgroundColor: this.props.backgroundColor
+    }
+    const onClickHandler = (event) => {
+      this.setState({ subMenuType: SubMenuType.BackgroundColor })
+    }
+    return (
+      <div>
+        <p>Background Color</p>
+        <div style={style} onClick={onClickHandler}>
+        </div>
+      </div>
+    )
+  }
+
+  getColorPalette (paletteName, onClickHandler) {
+    const colors = [
+      '#e60012', '#f39800', '#fff100', '#009944', '#0068b7', '#1d2088', '#920783'
+    ]
+    const colorBoxStyle = {
+      height: '30px',
+      width: '30px',
+      border: '1px solid #ccc'
+    }
+    const paletteStyle = {
+      right: 0,
+      top: '200px',
+      position: 'fixed',
+      width: '378px',
+      border: '1px solid #ccc',
+      zIndex: 10000,
+      backgroundColor: '#e8e8e8'
+    }
+    const colorNodes = colors.map((color, i) => {
+      const style = Object.assign({}, colorBoxStyle, { backgroundColor: color })
+      return <div key={i} style={style} onClick={onClickHandler}></div>
+    })
+    return (
+      <div style={paletteStyle}>
+        <p>{paletteName}</p>
+        {colorNodes}
+      </div>
+    )
+  }
+
+  getSubMenu () {
+    switch (this.state.subMenuType) {
+      case SubMenuType.LineColor:
+        return this.getColorPalette('Line Color', (event) => {
+          this.props.setLineColor(event.target.style.backgroundColor)
+        })
+      case SubMenuType.BackgroundColor:
+        return this.getColorPalette('Background Color', (event) => {
+          this.props.setBackgroundColor(event.target.style.backgroundColor)
+        })
+      default:
+        return null
+    }
+  }
+
   getCloseButton () {
     const onClickHandler = (event) => {
       this.props.hideMe()
@@ -58,10 +165,11 @@ class Menu extends Component {
 
   render () {
     if (this.props.isVisibleMenu) {
-      const style = {
+      const mainMenuStyle = {
         right: 0,
         top: 0,
         position: 'fixed',
+        height: '200px',
         width: '378px',
         border: '1px solid #ccc',
         borderTop: 0,
@@ -69,9 +177,14 @@ class Menu extends Component {
         backgroundColor: '#e8e8e8'
       }
       return (
-        <div style={style}>
-          {this.getExtensionSwitcher()}
-          {this.getCloseButton()}
+        <div>
+          <div style={mainMenuStyle}>
+            {this.getExtensionSwitcher()}
+            {this.getLineColorSelector()}
+            {this.getBackgroundColorSelector()}
+            {this.getCloseButton()}
+          </div>
+          {this.getSubMenu()}
         </div>
       )
     }
